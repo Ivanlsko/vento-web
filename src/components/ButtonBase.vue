6fde958e528bdecc957d47a43cd777468d51d607
Ivanlsko <il.ivanlesko@gmail.com>
2024-03-13 22:19:13 +0100

    remove unnecessary code

--- src/components/ButtonBase.vue
+++ src/components/ButtonBase.vue
@@ -1,24 +1,18 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 
+// to-do prerobit takto props
 defineProps({
-  content: String
-})
-
-// reactive state
-const count = ref(0)
-
-// functions that mutate state and trigger updates
-function increment() {
-  count.value++
-}
-
-// lifecycle hooks
-onMounted(() => {
-  console.log(`The initial count is ${count.value}.`)
+  content: String,
+  mode: String
 })
 </script>
 
 <template>
-  <button class="py-3 px-5 border rounded-full">{{ content }}</button>
+  <button
+    class="py-3 px-5 border rounded-full"
+    :class="mode === 'black' ? 'border-black' : 'border-white'"
+  >
+    {{ content }}
+  </button>
 </template>
