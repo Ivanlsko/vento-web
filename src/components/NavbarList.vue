07531bef1df4efc0fee7967a9cde99b28a23a068
Ivanlsko <il.ivanlesko@gmail.com>
2024-03-25 21:49:18 +0100

    add component

---
+++ src/components/NavbarList.vue
@@ -0,0 +1,5 @@
+<template>
+  <li class="mr-4"><a href="#events">Služby</a></li>
+  <li class="mr-4"><a href="#about">O nás</a></li>
+  <li class="mr-4"><a href="#people">Členovia</a></li>
+</template>
