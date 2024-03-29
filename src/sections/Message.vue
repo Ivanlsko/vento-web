d8d199209f08d29a7af1748f386a7ca33efdabdf
Ivanlsko <il.ivanlesko@gmail.com>
2024-03-29 10:37:46 +0100

    add slovak text

--- src/sections/Message.vue
+++ src/sections/Message.vue
@@ -3,12 +3,12 @@ import FormBase from '../components/FormBase.vue'
 </script>
 
 <template>
-  <section class="md:flex w-full gap-32">
+  <section id="message" class="md:flex w-full gap-32">
     <div class="md:w-1/2">
       <h2 class="text-5xl mb-12">Ozvite sa nám</h2>
       <p>
-        We are suprisingly bad in replying to emails but just say coffee meeting and we will send
-        you our favourite spot in London, Hong Kong or New York
+        Ak by ste sa snami radi dohodli na spolupráci, máte akúkoľvek otázku alebo Vás iba zaujíma
+        ako sa máme, pošite nám správu. Ozveme sa čo najskôr.
       </p>
     </div>
     <FormBase class="mt-20 md:w-1/2 md:mt-0"></FormBase>
