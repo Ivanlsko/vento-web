3f85c18233d7bec292c73d7ec4a4d8b4faaf9c06
Ivanlsko <il.ivanlesko@gmail.com>
2024-03-29 10:36:14 +0100

    add navigateToSection fn

--- src/components/ButtonBase.vue
+++ src/components/ButtonBase.vue
@@ -4,12 +4,21 @@ import { ref, onMounted } from 'vue'
 // to-do prerobit takto props
 defineProps({
   content: String,
+  sectionId: String,
   mode: String
 })
+
+function navigateToSection(id) {
+  const section = document.getElementById(id)
+  if (section) {
+    section.scrollIntoView()
+  }
+}
 </script>
 
 <template>
   <button
+    @click="sectionId && navigateToSection(sectionId)"
     class="py-3 px-5 border rounded-full"
     :class="mode === 'black' ? 'border-black' : 'border-white'"
   >
