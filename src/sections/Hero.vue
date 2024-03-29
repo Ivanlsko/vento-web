25365af4038c84d6bc0975d74cea84676adadb46
Ivanlsko <il.ivanlesko@gmail.com>
2024-03-29 10:37:17 +0100

    add button to mobile view, use images from server

--- src/sections/Hero.vue
+++ src/sections/Hero.vue
@@ -1,11 +1,13 @@
-<script setup></script>
+<script setup>
+import ButtonBase from '../components/ButtonBase.vue'
+</script>
 
 <template>
   <section
-    class="bg-[url('../assets/images/vertical-cover.jpg')] bg-top min-h-[1000px] lg:bg-[url('../assets/images/cover.jpg')] w-full h-screen bg-cover flex-center lg:bg-center bg-no-repeat"
+    class="bg-[url('https://vento.sk/media/vento_mobile.webp')] bg-top min-h-[1000px] lg:bg-[url('https://vento.sk/media/vento_desktop.webp')] w-full h-screen bg-cover flex-center bg-no-repeat lg:bg-auto"
   >
-    <div id="hero-content" class="text-white mx-8 lg:mx-24 mt-28">
-      <h1 class="col-span-2 text-5xl mb-20">
+    <div id="hero-content" class="text-white sm:mx-8 lg:mx-24 mt-28">
+      <h1 class="col-span-2 text-[3rem] sm:text-5xl -mb-4 sm:mb-20">
         Vento kvintet je dychové hudobné teleso tvorené piatimi profesionálmi.
       </h1>
       <div class="flex-col lg:flex lg:flex-row justify-between items-start">
@@ -13,11 +15,16 @@
           <div id="circle-available" class="w-2 h-2 rounded-full bg-white mr-2"></div>
           <p class="text-sm">Termíny sú k dispozícií</p>
         </div>
-        <p id="header-paragraph" class="lg:w-1/2 text-2xl mt-10 lg:mt-0">
+        <p id="header-paragraph" class="lg:w-1/2 text-2xl mt-4 sm:mt-10 lg:mt-0">
           Vašému podujatiu dodáme jedinečnú atmosféru, či už ide o svadbu, firemný večierok alebo
           vernisáž.
         </p>
       </div>
+      <ButtonBase
+        class="w-full mt-10 md:hidden"
+        :content="'Ozvite sa nám'"
+        :sectionId="'message'"
+      ></ButtonBase>
     </div>
   </section>
 </template>
